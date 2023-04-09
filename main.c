30a20715d00a70c8850aa21f89b8b07f5df8d497
ParameshwarGani <125493251+ParameshwarGani@users.noreply.github.com>
2023-04-09 19:45:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,13 @@
-
-#include<stdio.h>
-#include<stdlib.h>
-void display();
-struct node
-{
-	int data;
-	struct node *next;
-};
-struct node *start;
+#include <stdio.h>
 int main()
 {
-	struct node *newnode1 = (struct node*) malloc(sizeof(struct node));
-	struct node *newnode2 = (struct node*) malloc(sizeof(struct node));
-	start = newnode1;
-	newnode1->data = 2;
-	newnode1->next = newnode2;
-	newnode2->data = 3;
-	newnode2->next = NULL;
-	display();
-	return 0;
+	int x=4;
+	int *p;
+	p=&x; 
+	printf("Value at address pointed by p: %d \n",*p);
+	printf("Value of x: %d \n",x);
+	printf("Address of x: %d \n",&x);
+	printf("Value of p: %d \n",p);
+	printf("Address of p: %d \n",&p);
+return 0;
 }
-void display()
-{
-	struct node *temp;
-	temp = start;
-	while(temp!=NULL)
-	{
-		printf("data = %d, self addr = %d, next addr = %d\n",temp->data,temp,temp->next);
-		temp=temp->next; 
-	}
-}
-
-
-
